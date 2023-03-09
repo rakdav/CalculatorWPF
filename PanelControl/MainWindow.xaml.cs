8440689e646ec0ed65da90f4d4b4508992136a70
Rakdav <rakdav@inbox.ru>
2023-03-09 12:47:18 +0200

    Добавьте файлы проекта.

---
+++ PanelControl/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PanelControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //var button1=new Button() { Content = "Button 1" };
+            //var button2 = new Button() { Content = "Button 2" };
+            //var button3 = new Button() { Content = "Button 3" };
+            //var button4 = new Button() { Content = "Button 4" };
+            //var button5 = new Button() { Content = "Button 5" };
+            //var stackPanel=new StackPanel() { Orientation=Orientation.Horizontal};
+            //stackPanel.Children.Add(button1);
+            //stackPanel.Children.Add(button2);
+            //stackPanel.Children.Add(button3);
+            //stackPanel.Children.Add(button4);
+            //stackPanel.Children.Add(button5);
+            //mainGrid.Children.Add(stackPanel);
+        }
+    }
+}
